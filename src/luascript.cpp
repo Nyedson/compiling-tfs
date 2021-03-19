17f9fa768a2e797e5ae25faacb14b7e2cf61e81a
ZeroSkyer <70819533+ZeroSkyer@users.noreply.github.com>
2021-03-19 05:28:03 -0600

    g

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -3091,6 +3091,8 @@ void LuaScriptInterface::registerFunctions()
 	registerMethod("MonsterType", "isBlockable", LuaScriptInterface::luaMonsterTypeIsBlockable);
 
 	registerMethod("MonsterType", "isPet", LuaScriptInterface::luaMonsterTypeIsPet);
+	registerMethod("MonsterType", "isMonsterAttacker ", LuaScriptInterface::luaMonsterTypeIsMonsterAttacker );
+	registerMethod("MonsterType", "cantAttackPlayers ", LuaScriptInterface::luaMonsterTypeCantAttackPlayers );
 	registerMethod("MonsterType", "isRewardBoss", LuaScriptInterface::luaMonsterTypeIsRewardBoss);
 
 	registerMethod("MonsterType", "canSpawn", LuaScriptInterface::luaMonsterTypeCanSpawn);
@@ -15069,6 +15071,57 @@ int LuaScriptInterface::luaMonsterTypeIsHostile(lua_State* L)
 			pushBoolean(L, monsterType->info.isHostile);
 		} else {
 			monsterType->info.isHostile = getBoolean(L, 2);
+			pushBoolean(L, true);
+		}
+	} else {
+		lua_pushnil(L);
+	}
+	return 1;
+}
+
+int LuaScriptInterface::luaMonsterTypeIsPet(lua_State* L)
+{
+	// get: monsterType:isPet() set: monsterType:isPet(bool)
+	MonsterType* monsterType = getUserdata<MonsterType>(L, 1);
+	if (monsterType) {
+		if (lua_gettop(L) == 1) {
+			pushBoolean(L, monsterType->info.isPet);
+		} else {
+			monsterType->info.isPet = getBoolean(L, 2);
+			pushBoolean(L, true);
+		}
+	} else {
+		lua_pushnil(L);
+	}
+	return 1;
+}
+
+int LuaScriptInterface::luaMonsterTypeIsMonsterAttacker (lua_State* L)
+{
+	// get: monsterType:isPet() set: monsterType:isPet(bool)
+	MonsterType* monsterType = getUserdata<MonsterType>(L, 1);
+	if (monsterType) {
+		if (lua_gettop(L) == 1) {
+			pushBoolean(L, monsterType->info.isMonsterAttacker );
+		} else {
+			monsterType->info.isMonsterAttacker  = getBoolean(L, 2);
+			pushBoolean(L, true);
+		}
+	} else {
+		lua_pushnil(L);
+	}
+	return 1;
+}
+
+int LuaScriptInterface::luaMonsterTypeCantAttackPlayers (lua_State* L)
+{
+	// get: monsterType:isPet() set: monsterType:isPet(bool)
+	MonsterType* monsterType = getUserdata<MonsterType>(L, 1);
+	if (monsterType) {
+		if (lua_gettop(L) == 1) {
+			pushBoolean(L, monsterType->info.cantAttackPlayers );
+		} else {
+			monsterType->info.cantAttackPlayers  = getBoolean(L, 2);
 			pushBoolean(L, true);
 		}
 	} else {
