ce67c8cb86bc7f1cae686bc2969bfffe9b4b0aeb
Daniel Reyes <70819533+ZeroSkyer@users.noreply.github.com>
2021-07-17 21:46:54 -0500

    Update protocolgamebase.h

--- src/protocolgamebase.h
+++ src/protocolgamebase.h
@@ -111,7 +111,7 @@ class ProtocolGameBase : public Protocol {
 
 		Player* player = nullptr;
 		uint32_t eventConnect = 0;
-		uint32_t version = g_config.getNumber(ConfigManager::CLIENT_VERSION);
+		uint32_t version = 12.61;
 		int32_t clientVersion = 0;
 
 		uint32_t challengeTimestamp = 0;
