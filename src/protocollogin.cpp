d8d4a53eb694433362bec18d01978297aecf25f5
Daniel Reyes <70819533+ZeroSkyer@users.noreply.github.com>
2021-07-18 19:37:57 -0500

    Update protocollogin.cpp

--- src/protocollogin.cpp
+++ src/protocollogin.cpp
@@ -52,7 +52,7 @@ void ProtocolLogin::addWorldInfo(OutputMessage_ptr& output, const std::string& a
 {
 	const std::string& motd = g_config.getString(ConfigManager::MOTD);
 	if (!motd.empty()) {
-		//Add MOTD
+		// Add MOTD
 		output->addByte(0x14);
 
 		std::ostringstream ss;
@@ -60,16 +60,18 @@ void ProtocolLogin::addWorldInfo(OutputMessage_ptr& output, const std::string& a
 		output->addString(ss.str());
 	}
 
-	//Add session key
+	// Add session key
 	output->addByte(0x28);
 	output->addString(accountName + "\n" + password);
 
-	//Add char list
-	output->addByte(0x64);
+	// Add char list
+  std::vector<account::Player> players;
+  account.GetAccountPlayers(&players);
+  output->addByte(0x64);
 
-	output->addByte(1); // number of worlds
+  output->addByte(1);  // number of worlds
 
-	output->addByte(0); // world id
+	output->addByte(0);  // world id
 	output->addString(g_config.getString(ConfigManager::SERVER_NAME));
 	output->addString(g_config.getString(ConfigManager::IP));
 
@@ -108,34 +110,43 @@ void ProtocolLogin::getCastingStreamsList(const std::string& password, uint16_t 
 
 void ProtocolLogin::getCharacterList(const std::string& accountName, const std::string& password, uint16_t version)
 {
-	//dispatcher thread
-	Account account;
-	if (!IOLoginData::loginserverAuthentication(accountName, password, account)) {
+	// Load Account Information
+  int result = 0;
+  account::Account account;
+  result = account.LoadAccountDB(accountName);
+  if (result) {
+    return;
+  }
+
+  // Check Login Password
+	if (!IOLoginData::LoginServerAuthentication(accountName, password)) {
 		disconnectClient("Account name or password is not correct.", version);
 		return;
 	}
 
 	auto output = OutputMessagePool::getOutputMessage();
-	//Update premium days
+	// Update premium days
 	Game::updatePremium(account);
-
+	
 	addWorldInfo(output, accountName, password, version);
-	uint8_t size = std::min<size_t>(std::numeric_limits<uint8_t>::max(), account.characters.size());
+	uint8_t size = std::min<size_t>(std::numeric_limits<uint8_t>::max(), account.players.size());
 	output->addByte(size);
 	for (uint8_t i = 0; i < size; i++) {
 		output->addByte(0);
-		output->addString(account.characters[i]);
+		output->addString(account.players[i].name);
 	}
 
-	//Add premium days
+	// Add premium days
 	output->addByte(0);
 	if (g_config.getBoolean(ConfigManager::FREE_PREMIUM)) {
 		output->addByte(1);
 		output->add<uint32_t>(0);
 	} else {
-		output->addByte(0);
-		output->add<uint32_t>(time(nullptr) + (account.premiumDays * 86400));
-	}
+    uint32_t days;
+    account.GetPremiumRemaningDays(&days);
+    output->addByte(0);
+    output->add<uint32_t>(time(nullptr) + (days * 86400));
+  }
 
 	send(output);
 
