2b47e4692caaf8f1d1f925a44ffa2efa734e825c
Daniel Reyes <70819533+ZeroSkyer@users.noreply.github.com>
2021-11-04 05:00:05 -0600

    check creature & check decay

--- src/game.h
+++ src/game.h
@@ -533,8 +533,6 @@ class Game
 		Raids raids;
 		GameStore gameStore;
 
-		std::forward_list<Item*> toDecayItems;
-
 		std::forward_list<Item*> toImbuedItems;
 
 	private:
@@ -555,8 +553,10 @@ class Game
 		std::unordered_map<uint16_t, Item*> uniqueItems;
 		std::map<uint32_t, uint32_t> stages;
 
-		std::list<Item*> decayItems[EVENT_DECAY_BUCKETS];
-		std::list<Creature*> checkCreatureLists[EVENT_CREATURECOUNT];
+		std::vector<Item*> decayItems[EVENT_DECAY_BUCKETS];
+		std::vector<Creature*> checkCreatureLists[EVENT_CREATURECOUNT];
+
+		std::vector<Item*> toDecayItems;
 
 		std::list<Item*> imbuedItems[EVENT_IMBUEMENT_BUCKETS];
         
