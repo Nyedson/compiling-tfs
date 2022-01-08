27c6b824986523879f5b9061344abbed488a60bc
Daniel Reyes <70819533+ZeroSkyer@users.noreply.github.com>
2022-01-08 00:19:28 -0600

    Update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -438,7 +438,7 @@ void Game::onPressHotkeyEquip(Player* player, uint16_t spriteid)
 	if (value1 > 0) {
 		player->setMoveExhaust(value1);
 	} else {
-		player->setMoveExhaust(900);
+		player->setMoveExhaust(2000);
 	}
 
 	if (ret != RETURNVALUE_NOERROR) {
