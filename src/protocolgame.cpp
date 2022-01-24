024d45ce252ba562f1e11ebbb9e2b8c9e7254460
Daniel Reyes <70819533+ZeroSkyer@users.noreply.github.com>
2022-01-23 21:06:38 -0600

    Update protocolgame.cpp

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -383,10 +383,8 @@ void ProtocolGame::onRecvFirstMessage(NetworkMessage& msg)
 	}
 
 	if (!Protocol::RSA_decrypt(msg)) {
-		//std::cout << "[ProtocolLogin::onRecvFirstMessage] RSA Decrypt Failed" << std::endl;
-		std::ostringstream ss;
-		ss << "Your client version is outdated. Download a new version on our website.";
-		disconnectClient(ss.str(), version);
+		std::cout << "[ProtocolGame::onRecvFirstMessage] RSA Decrypt Failed" << std::endl;
+		disconnect();
 		return;
 	}
 
