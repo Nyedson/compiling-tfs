db4755b2900f71301e15ae77b87120cbc6607eb4
Daniel Reyes <70819533+ZeroSkyer@users.noreply.github.com>
2022-04-19 13:04:37 -0500

    resilience

--- src/player.cpp
+++ src/player.cpp
@@ -1277,16 +1277,6 @@ void Player::onRemoveCreature(Creature* creature, bool isLogout)
 			g_game.internalCloseTrade(this);
 		}
 
-		if (savedPlayerLevel != 0) {
-			level = savedPlayerLevel;
-			experience = savedPlayerExperience;
-			health = savedPlayerHP;
-			healthMax = savedPlayerMaxHP;
-			mana = savedPlayerMP;
-			manaMax = savedPlayerMaxMP;
-			savedPlayerLevel = 0;
-		}
-
 		closeShopWindow();
 
 		clearPartyInvitations();
